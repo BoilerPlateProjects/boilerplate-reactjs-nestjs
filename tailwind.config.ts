7349a0861db1585919bde05cd8a824f8a42a53f1
MDReal32 <veysaliyev00@gmail.com>
2024-04-22 11:07:14 +0400

    Clear unused code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,7 @@ import { Config } from "tailwindcss";
 export default {
   content: ["src/**/*.ts", "!src/api/**"],
   theme: {
-    extend: {
-      colors: {
-        base: "#121212"
-      }
-    }
+    extend: {}
   },
   plugins: []
 } satisfies Config;
